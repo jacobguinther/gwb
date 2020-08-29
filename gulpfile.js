66dc754ad2030d7e8d7e08904a040f0c7150b32e
Jacob Guinther <jacobguinther@Jacobs-iMac.local>
2020-08-28 20:39:15 -0600

    add sass and gulp

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+// const data = require('gulp-data');
+// const fs = require('fs');
+// const log = require('fancy-log');
+// const ejs = require('gulp-ejs');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+
+sass.compiler = require('node-sass');
+
+gulp.task('scss', function () {
+    return gulp
+      .src('./src/scss/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(
+        rename(function (path) {
+          path.basename = 'styles';
+        }),
+      )
+      .pipe(gulp.dest('./dist/css'));
+  });
+
+  gulp.task('watch', function () {
+    gulp.watch(
+      './src/scss/**/*.scss',
+      { ignoreInitial: false },
+      gulp.series('scss')
+    );
+    // gulp.watch(
+    //   ['./src/views/**/*.ejs', './src/data/**/*.json'],
+    //   { ignoreInitial: false },
+    //   gulp.series('ejs'),
+    // );
+  });

2b71c3cccae01fb12eaef8176a7bcf54538f4ae0
Jacob Guinther <jacobguinther@Jacobs-iMac.local>
2020-08-29 17:15:02 -0600

    add edge to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,17 @@ const gulp = require('gulp');
 // const fs = require('fs');
 // const log = require('fancy-log');
 // const ejs = require('gulp-ejs');
+const gulpEdge = require('gulp-edgejs');
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
 
 sass.compiler = require('node-sass');
+
+gulp.task('edge', ()=>{
+    return gulp.src('./src/views/*.edge')
+      .pipe( gulpEdge() )
+      .pipe(gulp.dest('./dist'));
+  });
 
 gulp.task('scss', function () {
     return gulp

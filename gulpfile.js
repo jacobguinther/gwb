c8213eca816fec6c82a18ed8dcdcd54ef7222fd4
Jacob Guinther <jacobguinther@Jacobs-iMac.local>
2020-08-31 11:57:56 -0600

    global data to edge

--- gulpfile.js
+++ gulpfile.js
@@ -2,41 +2,43 @@ const gulp = require('gulp');
 const gulpEdge = require('gulp-edgejs');
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
+const log = require('fancy-log');
+const data = require('./src/data/data')
 
 sass.compiler = require('node-sass');
 
-gulp.task('edge', ()=>{
-    return gulp.src('./src/views/*.edge')
-      .pipe( gulpEdge() )
-      .pipe(gulp.dest('./dist'));
-  });
+gulp.task('edge', () => {
+  return gulp.src('./src/views/*.edge')
+    .pipe(gulpEdge({globals: {data: data}}).on('error', log),)
+    .pipe(gulp.dest('./dist'));
+});
 
 gulp.task('scss', function () {
-    return gulp
-      .src('./src/scss/**/*.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(
-        rename(function (path) {
-          path.basename = 'styles';
-        }),
-      )
-      .pipe(gulp.dest('./dist/css'));
-  });
+  return gulp
+    .src('./src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(
+      rename(function (path) {
+        path.basename = 'styles';
+      }),
+    )
+    .pipe(gulp.dest('./dist/css'));
+});
 
-  gulp.task('watch', function () {
+gulp.task('watch', function () {
+  gulp.watch(
+    './src/scss/**/*.scss',
+    { ignoreInitial: false },
+    gulp.series('scss')
+  ),
     gulp.watch(
-      './src/scss/**/*.scss',
+      './src/views/**/*.edge',
       { ignoreInitial: false },
-      gulp.series('scss')
-    ),
-    gulp.watch(
-        './src/views/**/*.edge',
-        { ignoreInitial: false },
-        gulp.series('edge'),
+      gulp.series('edge'),
     );
-    // gulp.watch(
-    //   ['./src/views/**/*.ejs', './src/data/**/*.json'],
-    //   { ignoreInitial: false },
-    //   gulp.series('ejs'),
-    // );
-  });
+  // gulp.watch(
+  //   ['./src/views/**/*.ejs', './src/data/**/*.json'],
+  //   { ignoreInitial: false },
+  //   gulp.series('ejs'),
+  // );
+});
